ad0a187f3037b09c392cf2bc66ba93ab7eab1d99
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2017-08-29 16:58:05 +0300

    mbean

---
+++ src/main/scala/ru/tema/broker/MessageBrokerMBean.scala
@@ -0,0 +1,17 @@
+package ru.tema.broker
+
+import java.util
+
+trait MessageBrokerMBean {
+
+  // JMX attributes
+  def getQueuedMessagesCount: Int
+  def getSendersCount: Int
+  def getRecipientsCount: Int
+
+  // JMX operation
+  def senders: util.Set[String]
+  def recipients: util.Set[String]
+  def dropQueue(): Int
+  def lastMessages(n: Int): util.Set[String]
+}
